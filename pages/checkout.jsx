2cfc8716468bdd4b8a07955ec41407eb32a1a0be
M. Irfan <HP@VISERION.dev.mirfan@gmail.com>
2023-02-21 19:27:23 +0700

    fix error

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 import midtransClient from "midtrans-client";
 
-const checkout = () => {
-  //   Create Snap API instance
-  // let snap = new midtransClient.Snap({
-  //   isProduction: false,
-  //   clientKey: "SB-Mid-client-NkjrkemgHGwWQ-M1",
-  //   serverKey: "SB-Mid-server-mXb_D8mFlPQNihU74XM4oRjf",
-  // });
+export async function getServerSideProps() {
+  let snap = new midtransClient.Snap({
+    isProduction: false,
+    clientKey: "SB-Mid-client-NkjrkemgHGwWQ-M1",
+    serverKey: "SB-Mid-server-mXb_D8mFlPQNihU74XM4oRjf",
+  });
 
   let parameter = {
     transaction_details: {
@@ -25,49 +24,23 @@ const checkout = () => {
     },
   };
 
-  // snap.createTransaction(parameter).then((transaction) => {
-  //   // transaction token
-  //   let transactionToken = transaction.token;
-  //   console.log("transactionToken:", transactionToken);
-  // });
-
-  function buatPesanan(e) {
-    var data = JSON.stringify({
-      transaction_details: {
-        order_id: "ORDER-101-1676979994",
-        gross_amount: 10000,
+  const trancsaction = await snap.createTransaction(parameter).catch((err) => {
+    return null;
+  });
+  if (trancsaction)
+    return {
+      redirect: {
+        destination: trancsaction.redirect_url,
       },
-      credit_card: {
-        secure: true,
-      },
-      customer_details: {
-        first_name: "budi",
-        last_name: "pratama",
-        email: "budi.pra@example.com",
-        phone: "08111222333",
-      },
-    });
-
-    var config = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: "https://app.sandbox.midtrans.com/snap/v1/transactions",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization:
-          "Basic U0ItTWlkLXNlcnZlci1tWGJfRDhtRmxQUU5paFU3NFhNNG9SamY6",
-      },
-      data: data,
     };
 
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  return {
+    props: {},
+  };
+}
+
+const checkout = () => {
+  function buatPesanan(e) {
     alert("Buat Pesanan");
   }
   return (
